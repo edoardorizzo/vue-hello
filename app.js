c68c27b267092877d037275a69035da8e6aec4f2
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-28 15:16:59 +0200

    add image and h1 with data

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+/* 
+Stampare a schermo un messaggio all’interno di un :puntare_a_destra: h1 :puntare_a_sinistra: utilizzando una proprietá nell'oggetto restituito dalla funzione data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        description: 'Vue (pronounced /vjuː/, like view) is a JavaScript framework for building user interfaces. It builds on top of standard HTML, CSS, and JavaScript and provides a declarative and component-based programming model that helps you efficiently develop user interfaces, be they simple or complex.',
+        image: './img/evanyou.jpeg'
       }
     }
   }).mount('#app')
